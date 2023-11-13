edec2780fbcceccba7937ab324d9614b35ae58ef
Tyuiu.BorodulinMS@outlook.com <matweyborodulin@gmail.com>
2023-11-13 23:02:14 +0500

    Add project files.

---
+++ Tyuiu.BorodulinMS.Sprint5.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.BorodulinMS.Sprint5.Task0.V6.Lib;
+
+namespace Tyuiu.BorodulinMS.Sprint5.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\motya\source\repos\Tyuiu.BorodulinMS.Sprint5\Tyuiu.BorodulinMS.Sprint5.Task0.V6\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
